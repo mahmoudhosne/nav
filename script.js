a8b763751905378f7715a97931e3ea40a337630a
mahmoudhosne <86571817+mahmoudhosne@users.noreply.github.com>
2023-04-30 01:04:02 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+'use strict';
+let img = document.querySelector('.img-gold');
+
+function phones(phone) {
+  img.src = phone;
+}
+let container = document.querySelector('.container');
+console.log(container);
+function colors(color) {
+  container.style.background = color;
+}
